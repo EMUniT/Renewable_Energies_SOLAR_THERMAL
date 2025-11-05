059c0ac4a5bec39fb44e11c5c124eeab1dbe3438
Murgia Edoardo <edoardo.murgia@studenti.unitn.it>
2025-11-05 11:49:22 +0100

    Project Completed

--- Consumption_profiles.m
+++ Consumption_profiles.m
@@ -1,76 +1,64 @@
-%% Consumption profiles
+function [FR_consumption,TR_consumption,T_mains,Ta_sym,I_sym] = consumption_profiles(min,semester,I_feb,I_aug,Ta_feb,Ta_aug)
 
-% !! Select a seasons (1=winter; 2=summer)
-season = 1;
+    FR_consumption = 0*min;                   % Flow rate consumption [l/min]
+    TR_consumption = 0*min;                   % Temperature requested consumption [°C]
 
-time = (0:60*24)'; % min
-flow_rate_consumption = 0*time; % l/min
-temperature_req = 0*time; % °C
+    if semester == 1                          % Summer
 
-if season == 2 % winter
-
-    Tmains = 12; % ° C
-
-    flow_rate_consumption(60*7:60*7+8) = 10; % l/min shower 1
-    temperature_req(60*7:60*7+8) = 38; % °C
-
-    flow_rate_consumption(60*7.25:60*7.25+2) = 6; % l/min face washing 1
-    temperature_req(60*7.25:60*7.25+2) = 35; % °C
-
-    flow_rate_consumption(60*7.5:60*7.5+3) = 6; % l/min breakfast wash
-    temperature_req(60*7.5:60*7.5+3) = 32; % °C
-
-    flow_rate_consumption(60*13:60*13+3) = 6; % l/min lunch wash
-    temperature_req(60*13:60*13+3) = 40; % °C
-
-    flow_rate_consumption(60*16:60*16+1) = 6; % l/min face washing 2
-    temperature_req(60*16:60*16+1) = 35; % °C
-
-    flow_rate_consumption(60*20:60*20+5) = 6; % l/min dinner wash
-    temperature_req(60*20:60*20+5) = 42; % l/min dinner wash
-
-    flow_rate_consumption(60*21.5:60*21.5+7) = 10; % l/min shower 2
-    temperature_req(60*21.5:60*21.5+7) = 38; % °C
-
-else % summer
-
-    Tmains = 16; 
-
-    flow_rate_consumption(60*6:60*6+8) = 10; % l/min shower 1
-    temperature_req(60*6:60*6+8) = 36; % ° C
-
-    flow_rate_consumption(60*6.25:60*6.25+4) = 8; % l/min face washing 1
-    temperature_req(60*6.25:60*6.25+4) = 32; % ° C
-
-    flow_rate_consumption(60*6.5:60*6.5+3) = 6; % l/min breakfast wash
-    temperature_req(60*6.5:60*6.5+3) = 32; % ° C
-
-    flow_rate_consumption(60*12:60*12+3) = 6; % l/min lunch wash
-    temperature_req(60*12:60*12+3) = 40; % ° C
-
-    flow_rate_consumption(60*18:60*18+10) = 10; % l/min evenign shower
-    temperature_req(60*18:60*18+10) = 35; % ° C
-
-    flow_rate_consumption(60*19:60*19+5) = 6; % l/min dinner wash
-    temperature_req(60*19:60*19+5) = 40; % l/min dinner wash
-
-    flow_rate_consumption(60*20.5:60*20.5+8) = 10; % l/min shower 2
-    temperature_req(60*20.5:60*20.5+8) = 36; % ° C
+        T_mains = 16;
+    
+        FR_consumption(7*60:7*60+10) = 15;     % Morning shower: 10 [min] - 15 [l/min]
+        TR_consumption(7*60:7*60+10) = 32;     % Morning shower: 10 [min] - 32 [°C]
+    
+        FR_consumption(7.5*60:7.5*60+4) = 6;   % Morning tooth brushing: 4 [min] - 6 [l/min]
+        TR_consumption(7.5*60:7.5*60+4) = 27;  % Morning tooth brushing: 4 [min] - 27 [°C]
+    
+        FR_consumption(13*60:13*60+6) = 6;     % Lunch dishes washing: 6 [min] - 6 [l/min]
+        TR_consumption(13*60:13*60+6) = 40;    % Lunch dishes washing: 6 [min] - 40 [°C]
+    
+        FR_consumption(15*60:15*60+3) = 4;     % After nap face washing: 3 [min] - 4 [l/min]
+        TR_consumption(15*60:15*60+3) = 30;    % After nap face washing: 3 [min] - 30 [°C]
+    
+        FR_consumption(20.5*60:20.5*60+6) = 6; % Dinner dishes washing: 6 [min] - 6 [l/min]
+        TR_consumption(20.5*60:20.5*60+6) = 40;% Dinner dishes washing: 6 [min] - 40 [°C]
+    
+        FR_consumption(22*60:22*60+15) = 15;   % Morning shower: 15 [min] - 15 [l/min]
+        TR_consumption(22*60:22*60+15) = 34;   % Morning shower: 15 [min] - 34 [°C]
+    
+        I_sym = I_aug;
+        Ta_sym = Ta_aug;
+    
+    
+    elseif semester == 2                       % Winter
+    
+        T_mains = 12;
+    
+        FR_consumption(6.5*60:6.5*60+12) = 15; % Morning shower: 12 [min] - 15 [l/min]
+        TR_consumption(6.5*60:6.5*60+12) = 40; % Morning shower: 12 [min] - 40 [°C]
+    
+        FR_consumption(7*60:7*60+4) = 6;       % Morning tooth brushing: 4 [min] - 6 [l/min]
+        TR_consumption(7*60:7*60+4) = 32;      % Morning tooth brushing: 4 [min] - 32 [°C]
+    
+        FR_consumption(12.7*60:12.7*60+6) = 6; % Lunch dishes washing: 6 [min] - 6 [l/min]
+        TR_consumption(12.7*60:12.7*60+6) = 42;% Lunch dishes washing: 6 [min] - 42 [°C]
+    
+        FR_consumption(15*60:15*60+3) = 4;     % After nap face washing: 3 [min] - 4 [l/min]
+        TR_consumption(15*60:15*60+3) = 38;    % After nap face washing: 3 [min] - 38 [°C]
+    
+        FR_consumption(19.5*60:19.5*60+6) = 6; % Dinner dishes washing: 6 [min] - 6 [l/min]
+        TR_consumption(19.5*60:19.5*60+6) = 40;% Dinner dishes washing: 6 [min] - 40 [°C]
+    
+        FR_consumption(21*60:21*60+15) = 15;   % Morning shower: 15 [min] - 15 [l/min]
+        TR_consumption(21*60:21*60+15) = 42;   % Morning shower: 15 [min] - 42 [°C]
+    
+        I_sym = I_feb;
+        Ta_sym = Ta_feb;
+    
+    else 
+        
+        error('Error: WRONG value of "semester". Check the value in the script.'); % Error for semester != 1||2
+    
+    end
 
 end
 
-figure(2)
-ha(1)=subplot(2,1,1);
-hold on
-grid on
-plot(time/60,flow_rate_consumption,'k')
-ylabel('Flow rate (l/min)')
-
-ha(2)=subplot(2,1,2);
-hold on
-grid on
-plot(time/60,temperature_req,'k')
-ylabel('Temp (°C)')
-xlabel('hour')
-
-linkaxes(ha,'x')
